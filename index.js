1babf6cc07600a9e94a8416095a27bf11f86c60c
Artem Pashkov <42417878+AlucardYanas@users.noreply.github.com>
2022-11-08 14:00:14 +0300

    Add files via upload
    changed for each method to map

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
+// import * as addTask from '/data.js';
+
 function renderData(toDoList) {
     const container = document.getElementById("todo-app");
     container.innerHTML = "";
-    toDoList.forEach((element) => {
-        const newDiv = document.createElement('div');
-        newDiv.innerHTML = element.description;
-        container.append(newDiv);
-        let doneButton = document.createElement('div');
-        doneButton.textContent = 'Удалить'
-        doneButton.classList.add('delete-button')
-        container.append(doneButton)
-    });
+    const result = toDoList.map(item => item.attribute);
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = result.description;
+    newDiv.id = result.id;
+    container.append(newDiv);
+    // container.append(result);
+    let doneButton = document.createElement('div');    
+    doneButton.textContent = 'Удалить';
+    doneButton.classList.add('delete-button');
+    container.append(doneButton);
 }
 
 
 document.getElementById("button").addEventListener("click", () => {
-    addTask(document.getElementById("input").value)
+    addTask(document.getElementById("input").value);
     renderData(data);
 });
 
-document.getElementById("delete-button").addEventListener("click", () => {
-    deleteTask()
+const deleteButton = document.querySelectorAll('delete-button');
+
+deleteButton.addEventListener("click", () => {
+    deleteTask(newDiv.id);
     renderData(data);
 });
-
-
 
 
 renderData(data);
